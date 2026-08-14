e38c712cc6b58acc22e4f2f82eb3ec5adcbcffe1
developer-bappeda <gigihtrianung@gmail.com>
2026-08-14 13:40:56 +0700

    views gta

---
+++ app/Views/auth/loginview.php
@@ -0,0 +1,110 @@
+<?= $this->extend('auth/template/index'); ?>
+
+<?= $this->section('logincontent'); ?>
+<div class="container-xxl">
+    <div class="authentication-wrapper authentication-basic container-p-y">
+        <div class="authentication-inner">
+            <!-- Register -->
+            <div class="card">
+                <div class="card-body">
+                    <!-- Logo -->
+                    <div class="app-brand justify-content-center">
+                        <a href="#" class="app-brand-link gap-2">
+                            <img src="<?= base_url(); ?>/assets/img/icons/logo.png" class="circle-img" alt="logo">
+                            <span class="app-brand-text demo text-body fw-bolder">Sneat</span>
+                        </a>
+                    </div>
+                    <!-- /Logo -->
+                    <h4 class="mb-2">Welcome to Sneat! 👋</h4>
+                    <p class="mb-4">Please sign-in to your account and start the adventure</p>
+
+                    <p class="login-box-msg"><?= lang('Auth.loginTitle') ?></p>
+
+                    <?= view('Myth\Auth\Views\_message_block') ?>
+                    <form class="mb-3" action="<?= route_to('login') ?>" method="POST">
+                        <?= csrf_field() ?>
+
+                        <?php if ($config->validFields === ['email']) : ?>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input
+                                    type="text"
+                                    class="form-control <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>" name="login" placeholder="<?= lang('Auth.email') ?>"
+                                    id="email"
+                                    name="email-username"
+                                    placeholder="Enter your email or username"
+                                    autofocus />
+
+                                <div class="invalid-feedback">
+                                    <?= session('errors.login') ?>
+                                </div>
+
+                            </div>
+                        <?php else : ?>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email or Username</label>
+                                <input
+                                    type="text"
+                                    class="form-control <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>" name="login" placeholder="<?= lang('Auth.emailOrUsername') ?>"
+                                    id="email"
+                                    name="email-username"
+                                    placeholder="Enter your email or username"
+                                    autofocus />
+
+                                <div class="invalid-feedback">
+                                    <?= session('errors.login') ?>
+                                </div>
+
+                            </div>
+                        <?php endif; ?>
+
+                        <div class="mb-3 form-password-toggle">
+                            <div class="d-flex justify-content-between">
+                                <label class="form-label" for="password">Password</label>
+                            </div>
+                            <div class="input-group input-group-merge">
+                                <input
+                                    type="password"
+                                    id="password"
+                                    class="form-control <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" placeholder="<?= lang('Auth.password') ?>"
+                                    name="password"
+                                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                    aria-describedby="password" />
+                                <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                            </div>
+
+                            <div class="invalid-feedback">
+                                <?= session('errors.password') ?>
+                            </div>
+                        </div>
+                        <?php if ($config->allowRemembering) : ?>
+                            <div class="mb-3">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="remember-me" <?php if (old('remember')) : ?> checked <?php endif ?> />
+                                    <?= lang('Auth.rememberMe') ?>
+                                </div>
+                            </div>
+                        <?php endif; ?>
+                        <div class="mb-3">
+                            <button class="btn btn-primary d-grid w-100" type="submit"><?= lang('Auth.loginAction') ?></button>
+                        </div>
+                    </form>
+
+                    <?php if ($config->allowRegistration) : ?>
+                        <p class="mb-0">
+                            <a href="<?= route_to('register') ?>" class="text-center"><?= lang('Auth.needAnAccount') ?></a>
+                        </p>
+                    <?php endif; ?>
+                    <?php if ($config->activeResetter) : ?>
+                        <p class="mb-0">
+                            <a href="<?= route_to('forgot') ?>" class="text-center"><?= lang('Auth.forgotYourPassword') ?></a>
+                        </p>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <!-- /Register -->
+        </div>
+    </div>
+</div>
+<!-- / Content -->
+<?= $this->endSection(); ?>
---
+++ app/Views/layouts/navbarsidebar.php
@@ -0,0 +1,78 @@
+<!-- Menu -->
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+    <div class="app-brand demo">
+        <a href="/" class="app-brand-link">
+
+            <img src="<?= base_url(); ?>/assets/img/icons/logo.png" class="circle-img" alt="logo">
+
+            <span class="app-brand-text demo menu-text fw-bolder ms-2">gta</span>
+        </a>
+
+        <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+            <i class="bx bx-chevron-left bx-sm align-middle"></i>
+        </a>
+    </div>
+
+    <div class="menu-inner-shadow"></div>
+
+    <ul class="menu-inner py-1">
+        <!-- Dashboard -->
+        <li class="menu-item active open">
+            <a href="/" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div data-i18n="Analytics">Dashboard</div>
+            </a>
+        </li>
+
+        <li class="menu-header small text-uppercase">
+            <span class="menu-header-text">Pages</span>
+        </li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-dock-top"></i>
+                <div data-i18n="Account Settings">Account Settings</div>
+            </a>
+            <ul class="menu-sub">
+                <li class="menu-item">
+                    <a href="pages-account-settings-account.html" class="menu-link">
+                        <div data-i18n="Account">Account</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="pages-account-settings-notifications.html" class="menu-link">
+                        <div data-i18n="Notifications">Notifications</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="pages-account-settings-connections.html" class="menu-link">
+                        <div data-i18n="Connections">Connections</div>
+                    </a>
+                </li>
+            </ul>
+        </li>
+
+        <!-- Forms & Tables -->
+        <li class="menu-header small text-uppercase"><span class="menu-header-text">Forms &amp; Tables</span></li>
+        <!-- Forms -->
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-detail"></i>
+                <div data-i18n="Form Elements">Form Elements</div>
+            </a>
+            <ul class="menu-sub">
+                <li class="menu-item">
+                    <a href="forms-basic-inputs.html" class="menu-link">
+                        <div data-i18n="Basic Inputs">Basic Inputs</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="forms-input-groups.html" class="menu-link">
+                        <div data-i18n="Input groups">Input groups</div>
+                    </a>
+                </li>
+            </ul>
+        </li>
+
+    </ul>
+</aside>
+<!-- / Menu -->
